a7fc2da269545558f9f34d53ca078b5a29bf0b82
Albub-Yusuf <albubyusuf@gmail.com>
2023-07-10 14:04:59 +0600

    Models relation made

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -21,25 +23,22 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'img'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    function profile():HasOne{
+
+        return $this->hasOne(Profile::class);
+
+    }
+
+    function posts():HasMany{
+        return $this->hasMany(Post::class);
+    }
+
+    function comments():HasMany{
+        return $this->hasMany(Comment::class);
+    }
+
 }
